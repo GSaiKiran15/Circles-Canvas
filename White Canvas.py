c31139b10d1252c02a0f76931f2134203ad713c4
Sai Kiran Ganesh Kumar <ganeshsaikiran7@gmail.com>
2024-01-25 12:10:46 -0700

    Few Updates

--- White Canvas.py
+++ White Canvas.py
@@ -7,12 +7,16 @@ center = (100,100)
 color = (0,0,255)
 radius = 100
 thickness = 3
-
+pressed = False
 def mouse_click(event, x,y,flags,params):
-    global center
+    global center, pressed
     if event == cv2.EVENT_LBUTTONDOWN:
         center = x,y
-
+        pressed = True
+    elif event == cv2.EVENT_MOUSEMOVE and pressed:
+        cv2.circle(canvas, (x,y), radius, color, thickness)
+    elif event == cv2.EVENT_LBUTTONDOWN:
+        pressed = False
 cv2.namedWindow("Canvas")
 cv2.setMouseCallback("Canvas", mouse_click)
 
@@ -24,6 +28,8 @@ while True:
         color = (255, 0, 0)
     elif ch == ord('g'):
         color = (0,255, 0)
+    elif ch == ord('r'):
+        color = (0,0,255)
     elif ch == ord('q'):
         break
 canvas.release()
